3fcec9afe2578149cdc219cf31ffd93c9621e826
Elia Martinez <eliahaydeemartinez@gmail.com>
2022-05-11 14:56:05 -0500

    updated link

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,44 +19,48 @@ var searchHandler = function (event) {
 
         var cityListItemEl = document.createElement('button')
         cityListItemEl.textContent = cityEntered;
-        cityListItemEl.setAttribute("style", "display: block; margin-bottom: 7px")
+        cityListItemEl.classList.add('btn', 'btn-info')
         cityPicked.appendChild(cityListItemEl);
 
     }
 
 };
 
-// var latLon = 'https://api.openweathermap.org/geo/1.0/direct?q='
+var latLon = 'https://api.openweathermap.org/geo/1.0/direct?q='
+// var cityurl = https://api.openweathermap.org/data/2.5/forecast?q='
+var oneCall = 'https://api.openweathermap.org/data/2.5/onecall?'
 
 
 var getCityWeather = function (city) {
 
-    var apiURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&limit=5&appid=4fde54c156119a215dce015fbaeecce2&units=metric'
+    var cityCord = latLon + city + '&limit=5&appid=4fde54c156119a215dce015fbaeecce2&units=metric'
+    var cityURL = oneCall + city + '&limit=5&appid=4fde54c156119a215dce015fbaeecce2&units=metric'
 
-    fetch(apiURL)
+
+    fetch(cityCord)
         .then(function (response) {
             if (response.ok) {
-            console.log(response);
-            response.json().then(function (data) {
-                console.log(data);
-                displayCurrentWeather();
-            });
-        }   
-})
+                console.log(response);
+                response.json()
+            
 
+                    .then(function (data) {
+                        console.log(data);
+                    });
+            }
+        })
 };
+
+
 
 searchBtn.addEventListener('click', searchHandler);
 
 
-function displayCurrentWeather (data) {
+function displayCurrentWeather(data) {
     console.log(data);
     var oneDay = document.getElementById("one-container");
 
     var currentWeather = data.list[0].weather[0].id
     var currentTemp = data.list.main
 
-
-
 }
-
