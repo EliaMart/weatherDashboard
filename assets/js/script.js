3a90f2fed04f7894063c3c961ba1d45a66948642
Elia Martinez <eliahaydeemartinez@gmail.com>
2022-05-12 14:39:31 -0500

    current weathe rappears

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var cityPicked = document.querySelector('#city-picked')
 var weatherContainer = document.querySelector('#weather-container')
 
 
-var searchHandler = function (event) {
+ function searchHandler (event) {
     event.preventDefault();
 
     var cityEntered = cityInputEl.value.trim();
@@ -15,7 +15,6 @@ var searchHandler = function (event) {
         getCityWeather(cityEntered);
 
         cityInputEl.value = '';
-
 
         var cityListItemEl = document.createElement('button')
         cityListItemEl.textContent = cityEntered;
@@ -31,9 +30,9 @@ var oneCall = 'https://api.openweathermap.org/data/2.5/onecall?'
  var weather = 'https://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=imperial&appid=${apiKey}'
 
 
-var getCityWeather = function (city) {
+function getCityWeather (city) {
 
-    var apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=4fde54c156119a215dce015fbaeecce2&units=imperial&cnt=5'
+    var apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=4fde54c156119a215dce015fbaeecce2&units=imperial'
 
     fetch(apiURL)
         .then(function (response) {
@@ -42,39 +41,46 @@ var getCityWeather = function (city) {
                 return response.json()
             }}) .then(function (data) {
                 console.log(data);
+                var lat = data.coord.lat;
+                var lon = data.coord.lon;
+                var getUv = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly&appid=4fde54c156119a215dce015fbaeecce2'
                 
-                // var dailyWeatherContainer = document.querySelector("#one-container")
-                // var date = new Date().toLocaleDateString();
-                // var main = data.main;
-                // var temp = Math.floor(main.temp);
-                // var humidity = main.humidity;
-                // var windSpeed = data.wind.speed;
-                // var icon = data.weather[0].icon
-                var lat = data.lat;
-                var lon = data.lon;
-                var getUv = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=4fde54c156119a215dce015fbaeecce2'
+                
                 fetch(getUv).then(function(response){
                 console.log(response)
                 response.json().then(function(data){
-                setUv = data.value
+                console.log(data);
+                // setUv = data.value
+                displayCurrentWeather(data);
                     });
             })
+
         })
 };
       
-
-
-
-
 
 searchBtn.addEventListener('click', searchHandler);
 
 
 function displayCurrentWeather(data) {
-    console.log(data);
-    var oneDay = document.getElementById("one-container");
 
-    var currentWeather = data.list[0].weather[0].id
-    var currentTemp = data.list.main
+    var currentWeatherContainer = document.querySelector("#one-container")
+    var name = cityInputEl.value;
+    var date = new Date().toLocaleDateString();
+    var temp = (data.daily[0].temp.day);
+    console.log(temp);
+    var humidity = data.daily[0].humidity;
+    var windSpeed = data.daily[0].wind_speed;
+    var icon = data.daily[0].weather[0].icon
+    console.log(icon)
+    var UV = data.daily[0].uvi;
 
-}
+    document.querySelector('.card-title').innerText =  name + date;
+    // document.getElementById('#weather-icon').src = 'http://openweathermap.org/img/w/' + icon + '.png';
+    document.querySelector('.card-text-T').innerText = 'Temp: ' + temp + ' \u00B0F'; 
+    document.querySelector('.card-text-W').innerText = 'Wind: ' + windSpeed + ' MPH';
+    document.querySelector('.card-text-H').innerText = 'Humidity: ' + humidity + '%';
+    document.querySelector('.card-text-U').innerText = 'UV Index: ' + UV;
+
+};
+
