b94e1192135300783e8d4a2785d7ad9cb369150a
Elia Martinez <eliahaydeemartinez@gmail.com>
2022-05-17 17:31:21 -0500

    5-day weather working with colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var cityPicked = document.querySelector('#city-picked')
 var weatherContainer = document.querySelector('#weather-container')
 
 
- function searchHandler (event) {
+function searchHandler (event) {
     event.preventDefault();
 
     var cityEntered = cityInputEl.value.trim();
@@ -18,17 +18,15 @@ var weatherContainer = document.querySelector('#weather-container')
 
         var cityListItemEl = document.createElement('button')
         cityListItemEl.textContent = cityEntered;
-        cityListItemEl.classList.add('btn', 'btn-info')
+        cityListItemEl.classList.add('btn', 'btn-info', 'mt-3')
+        cityListItemEl.setAttribute('style', "display: block")
         cityPicked.appendChild(cityListItemEl);
 
     }
 
+    // localStorage.getItem(data);
+
 };
-
-var latLon = 'https://api.openweathermap.org/geo/1.0/direct?q='
-var oneCall = 'https://api.openweathermap.org/data/2.5/onecall?'
- var weather = 'https://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=imperial&appid=${apiKey}'
-
 
 function getCityWeather (city) {
 
@@ -50,8 +48,8 @@ function getCityWeather (city) {
                 console.log(response)
                 response.json().then(function(data){
                 console.log(data);
-                // setUv = data.value
                 displayCurrentWeather(data);
+                fiveDayForecast(data);
                     });
             })
 
@@ -62,25 +60,91 @@ function getCityWeather (city) {
 searchBtn.addEventListener('click', searchHandler);
 
 
-function displayCurrentWeather(data) {
+function displayCurrentWeather(data, cityEntered) {
 
-    var currentWeatherContainer = document.querySelector("#one-container")
-    var name = cityInputEl.value;
+    var name = cityEntered;
+    console.log(name);
     var date = new Date().toLocaleDateString();
-    var temp = (data.daily[0].temp.day);
+    var temp = (data.current.temp);
     console.log(temp);
-    var humidity = data.daily[0].humidity;
-    var windSpeed = data.daily[0].wind_speed;
-    var icon = data.daily[0].weather[0].icon
+    var humidity = data.current.humidity;
+    var windSpeed = data.current.wind_speed;
+    var icon = data.current.weather[0].icon
     console.log(icon)
-    var UV = data.daily[0].uvi;
+    var UV = data.current.uvi;
+
 
     document.querySelector('.card-title').innerText =  name + date;
-    // document.getElementById('#weather-icon').src = 'http://openweathermap.org/img/w/' + icon + '.png';
+    // document.getElementById('#weather-icon').innerHTML = "http://openweathermap.org/img/w/" + icon + ".png" alt="img">;
     document.querySelector('.card-text-T').innerText = 'Temp: ' + temp + ' \u00B0F'; 
     document.querySelector('.card-text-W').innerText = 'Wind: ' + windSpeed + ' MPH';
     document.querySelector('.card-text-H').innerText = 'Humidity: ' + humidity + '%';
     document.querySelector('.card-text-U').innerText = 'UV Index: ' + UV;
 
+    // if (UV <= 2 ) {
+    //     var UVElement = document.getElementById(".UVIndex")
+    //     console.log(UVElement);
+    //     UVElement.setAttribute("class", "bg-success text-white");
+
+    //  }else if (UV > 3 && UV < 5) {        
+    //         UVElement.setAttribute("class", "bg-warning text-white")  
+    // } else {
+    //     UVElement.setAttribute("class", "bg-danger text-white")
+    // };
+
+
+    // localStorage.setItem(data);
+
 };
+
+function fiveDayForecast (data) {
+    let test = data;
+    console.log(test);
+
+    let boxes = document.querySelectorAll(".weather")
+
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].innerHTML="";
+
+        var fiveDayInfo ={
+            fiveTemp: data.daily[i].temp.day,
+            fiveHumidity: data.daily[i].humidity,
+            fiveWind: data.daily[i].wind_speed,
+            fiveDate: new Date(data.daily[i].dt * 1000),
+        }
+
+
+        var fiveDayDate = document.createElement("p");
+        fiveDayDate.setAttribute("class", "text-white");
+        fiveDayDate.innerHTML = fiveDayInfo.fiveDate.toDateString();
+        boxes[i].appendChild(fiveDayDate);
+
+
+        var fiveDayTemp = document.createElement("p");
+        fiveDayTemp.setAttribute("class", "text-white");
+        fiveDayTemp.innerHTML = "Temperature: " + fiveDayInfo.fiveTemp + ' \u00B0F';
+        boxes[i].appendChild(fiveDayTemp);
+
+
+        var fiveDayHumidity = document.createElement('p')
+        fiveDayHumidity.setAttribute("class", "text-white");
+        fiveDayHumidity.innerHTML = "Humidity: " + fiveDayInfo.fiveHumidity + "%";
+        boxes[i].appendChild(fiveDayHumidity);
+
+
+        var fiveDayWind = document.createElement('p')
+        fiveDayWind.setAttribute("class", "text-white");
+        fiveDayWind.innerHTML = "Wind: " + fiveDayInfo.fiveWind + "MPH";
+        boxes[i].appendChild(fiveDayWind);
+
+    
+
+
+        
+    };
+    
+};
+
+
+
 
